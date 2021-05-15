bb307ba025a5177dd34e55a40afc367752933ff1
jan0660 <jan0660@tutanota.com>
2021-05-15 14:32:10 +0200

    Initial commit

---
+++ Janit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+string[] Stages = {"/etc/runit/1", "/etc/runit/2", "/etc/runit/3"};
+string shell = "/bin/sh";
+
+Console.WriteLine("Janit v0.1");
+for (int i = 1; i <= Stages.Length; i++)
+{
+    Console.WriteLine($"enter stage: {i}");
+    var process = Process.Start(new ProcessStartInfo()
+    {
+        FileName = Stages[i],
+        Arguments = "0"
+    });
+    try
+    {
+        await process.WaitForExitAsync();
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine(exc);
+    }
+    Console.WriteLine($"leave stage: {i}");
+}
